5ce7d3435bbb23819834cb79fbf199b6c21ea0f6
max Ambrogio <86671808+Max-Ambrogio@users.noreply.github.com>
2022-01-27 10:54:23 -0500

    added arrow key controls as well as slide controls for mobile.

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-
+ 
 let makeSlideshow = function(slideshowParentSelector){
     
     let slides = document.querySelectorAll(slideshowParentSelector + ' img')
@@ -37,6 +37,75 @@ let makeSlideshow = function(slideshowParentSelector){
         doSlideTransitionBack()
     })
 
+    //arrow controls
+    document.onkeydown = checkKey;
+
+        function checkKey(e) {
+
+            e = e || window.event;
+
+            if (e.keyCode == '38') {
+                console.log("up arrow") 
+            }
+            else if (e.keyCode == '40') {
+                console.log("down arrow")
+            }
+            else if (e.keyCode == '37') {
+                doSlideTransitionBack()
+                console.log("left arrow")
+            }
+            else if (e.keyCode == '39') {
+                doSlideTransition()
+                console.log("right arrow")
+            }
+        }
+
+    //swipe controls
+    document.addEventListener('touchstart', handleTouchStart, false);        
+    document.addEventListener('touchmove', handleTouchMove, false);
+
+    var xDown = null;                                                        
+    var yDown = null;
+
+    function getTouches(evt) {
+        return evt.touches ||      
+        evt.originalEvent.touches; 
+    }                                                     
+
+    function handleTouchStart(evt) {
+        const firstTouch = getTouches(evt)[0];                                      
+        xDown = firstTouch.clientX;                                      
+        yDown = firstTouch.clientY;                                      
+    };                                                
+
+    function handleTouchMove(evt) {
+        if ( ! xDown || ! yDown ) {
+            return;
+        }
+
+        var xUp = evt.touches[0].clientX;                                    
+        var yUp = evt.touches[0].clientY;
+
+        var xDiff = xDown - xUp;
+        var yDiff = yDown - yUp;
+
+        if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {  /*main swipes*/
+            if ( xDiff > 0 ) {
+                doSlideTransitionBack() /* left swipe */ 
+            } else {
+                doSlideTransition() /* right swipe */
+            }                       
+            } else {
+            if ( yDiff > 0 ) {
+                /* up swipe */ 
+            } else { 
+                /* down swipe */
+            }                                                                 
+        }
+        /* reset values */
+        xDown = null;
+        yDown = null;                                             
+    };
 }
 
 
